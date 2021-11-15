0bd4361fa52d4cb3100761b7cc4a44a2e5814ca5
齋木一晴 <120420issei@gmail.com>
2021-11-15 20:57:33 +0900

    フォームを追加

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app  = express();
 
 app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
 
 const mysql = require('mysql');
 
@@ -20,11 +21,22 @@ app.get('/add', (req, res) => {
     res.render('add.ejs');
 });
 
+app.post('/add_word', (req, res) => {
+    //追加された単語を出力
+    console.log(req.body.questionName);
+    //単語リストを表示する処理
+    connection.query(
+        'select * from list',
+        (error, results) => {
+            res.render('index.ejs', {list: results});
+        }
+    )
+});
+
 app.get('/index', (req, res) => {
     connection.query(
         'select * from list',
         (error, results) => {
-            console.log(results);
             res.render('index.ejs', {list: results});
         }
     )
